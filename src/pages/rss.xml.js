e6bb8e0175d30b7c40d66b28da7e1591384ddcc6
0xMahabub <mahabub212002072@gmail.com>
2022-09-13 18:33:24 +0600

    rss:fixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,6 +6,6 @@ export const get = () =>
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		articles: import.meta.glob('./blog/**/*.md'),
+		items: import.meta.glob('./blog/**/*.md'),
 		projects: import.meta.glob('./project/**/*.md'),
 	});
